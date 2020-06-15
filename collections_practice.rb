6aed22d452cd7b6e7d951527d26834f954554f5d
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-15 17:02:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+def sort_array_asc(array)
+  new_array = []
+  new_array = array.sort
+  return new_array
+end
 
+
+def sort_array_desc(array)
+  new_array = array
+  new_array.sort!  { |a, b| b <=> a}
+  return new_array
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+   a.length <=> b.length
+  end
+end
+
+
+def swap_elements(array)
+  swap_elements_from_to(array,1,2)
+end
+
+
+def swap_elements_from_to(array, index, destination_index)
+  first_element = array[index]
+  second_element = array[destination_index]
+  array[destination_index] = first_element
+  array[index] = second_element
+  return array
+end
+
+
+def reverse_array(array)
+  new_array = []
+  while array.length != 0
+    new_array << array.pop
+  end
+  return new_array
+end
+
+
+def kesha_maker(array)
+  
+end
+
+
+def find_a(array)
+  
+end
+
+
+def add_s(array)
+  
+end
